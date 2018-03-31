fefffa7cf2a17b355855c0bac4b80b2821e4b82b
Unknown <padma_krithika@hotmail.com>
2018-03-30 22:53:45 -0700

    7th change commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,15 @@
 document.getElementById("g05button").onclick = function () {
-    location.href = "discover.html";
+    location.href = "../../discover.html";
 };
 
 document.getElementById("row3btn1").onclick = function () {
-    location.href = "partypacks.html";
+    location.href = "../partypacks.html";
 };
 
 document.getElementById("row3btn2").onclick = function () {
-    location.href = "gifts.html";
+    location.href = "../gifts.html";
 };
 
 document.getElementById("row3btn3").onclick = function () {
-    location.href = "b2b.html";
+    location.href = "../b2b.html";
 };
