b082f104892ef23030d5f1db6c31ea73786763d2
Unknown <padma_krithika@hotmail.com>
2018-04-12 10:49:21 -0700

    10th commit

--- public/js/index.js
+++ public/js/index.js
@@ -13,3 +13,38 @@ document.getElementById("row3btn2").onclick = function () {
 document.getElementById("row3btn3").onclick = function () {
     location.href = "./b2b.html";
 };
+
+document.getElementById("row3mbtn1").onclick = function () {
+    location.href = "./partypacks.html";
+};
+
+document.getElementById("row3mbtn2").onclick = function () {
+    location.href = "./gifts.html";
+};
+
+document.getElementById("row3mbtn3").onclick = function () {
+    location.href = "./b2b.html";
+};
+
+document.getElementById("row4btn4").onclick = function () {
+    location.href = "./events.html";
+};
+
+document.getElementById("row4btn5").onclick = function () {
+    location.href = "./about.html";
+};
+
+document.getElementById("row4btn6").onclick = function () {
+    location.href = "./discover.html";
+};
+document.getElementById("row4mbtn4").onclick = function () {
+    location.href = "./events.html";
+};
+
+document.getElementById("row4mbtn5").onclick = function () {
+    location.href = "./about.html";
+};
+
+document.getElementById("row4mbtn6").onclick = function () {
+    location.href = "./discover.html";
+};
